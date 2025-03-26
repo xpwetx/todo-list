06602322f6cce5e6628d832f90303bd0e3f82725
Cakes <kathryjmiranda@gmail.com>
2025-03-25 22:02:46 -0700

    Week 2 hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import './App.css'
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function App() {
-  const todos = [
-    {id: 1, title: "review reserouces"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-
-  ]
-  return(
+  return (
     <div>
       <h1>My Todos</h1>
-      <ul>
-        {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
-      </ul>
+      <TodoForm />
+      <TodoList />
     </div>
   );
 }
 
-export default App
+export default App;
