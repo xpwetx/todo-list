a3fd103c759a6a13003c725d749076718e68601c
Cakes <kathryjmiranda@gmail.com>
2025-04-01 22:40:44 -0700

    week 3 hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-
-import './App.css'
-import TodoList from './TodoList'
-import TodoForm from './TodoForm'
+import React, { useState } from 'react';
+import './App.css';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('');
   return (
     <div>
       <h1>My Todos</h1>
+
+      {/*Input field to add new todo item*/}
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Enter a new todo"
+      />
+
+      <button onClick={() => console.log(newTodo)}>Add Todo</button>
+      <p>{newTodo}</p>
+
       <TodoForm />
       <TodoList />
     </div>
