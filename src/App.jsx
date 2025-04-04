bd4a6a9b0c657c2a7d64964228ce45e023235913
Cakes <kathryjmiranda@gmail.com>
2025-04-04 13:46:07 -0700

    resubmitting

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,11 @@ function App() {
   const [newTodo, setNewTodo] = useState('');
   return (
     <div>
-      <h1>My Todos</h1>
-
-      {/*Input field to add new todo item*/}
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Enter a new todo"
-      />
-
-      <button onClick={() => console.log(newTodo)}>Add Todo</button>
-      <p>{newTodo}</p>
-
+    
+  
       <TodoForm />
       <TodoList />
-    </div>
+      </div>
   );
 }
 
