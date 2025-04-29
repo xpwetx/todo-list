db221818a9a95aca81c514ecec47c314b2d0184c
Cakes <kathryjmiranda@gmail.com>
2025-04-29 12:55:14 -0700

    airtable stuff

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 const TextInputWithLabel = ({ elementId, labelText, onChange, inputRef, value }) => {
     return (
-        <>
+        <div className="input-group">
         <label htmlFor={elementId}>{labelText}</label>
         <input 
         id={elementId}
         type="text"
         onChange={onChange}
         ref={inputRef}
-        value={value} />
-        </>
+        value={value}
+         />
+        </div>
     );
 };
 
